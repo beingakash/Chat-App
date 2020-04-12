0c4b10fff926a862b518f154866bbb7779e477b8
beingAkash <beingakki26@gmail.com>
2020-04-13 04:20:26 +0530

    Third

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path=require('path')
 const http=require('http')
 const express=require('express')
 const socketio=require('socket.io')
+const Filter=require('bad-words')
 
 
 const app = express()
@@ -11,14 +12,45 @@ const io=socketio(server)
 
 const port= process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
-
-
-
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection',()=>{
+
+
+
+
+io.on('connection',(socket)=>{
    console.log('New Web Socket Connection')
+
+   socket.emit('message', 'Welcome!!')   
+   socket.broadcast.emit('message','A New User Has Joined')
+
+   socket.on('sendMessage',(message,callback)=>{
+
+        const filter= new Filter()
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed!')
+        }
+        io.emit('message',message)        
+        callback()
+
+   })
+
+   socket.on('sendLocation',(coords,callback)=>{
+       io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+       callback() 
+   })
+
+   socket.on('disconnect',()=>{
+
+    io.emit('message','User Has Left')
+   })
 })
+
+
+
+
+
+
 
 server.listen(port, () => {
 
