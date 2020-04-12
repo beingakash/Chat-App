74e4fd8698247928951dc2501fa1d2d90aa98835
beingAkash <beingakki26@gmail.com>
2020-04-13 00:09:00 +0530

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const path=require('path')
-
+const http=require('http')
 const express=require('express')
+const socketio=require('socket.io')
+
 
 const app = express()
+const server=http.createServer(app)
+const io=socketio(server)
 
 
 const port= process.env.PORT || 3000
@@ -12,7 +16,11 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection',()=>{
+   console.log('New Web Socket Connection')
+})
+
+server.listen(port, () => {
 
     console.log('Server is on PORT : '+port)
 })
